6508258060c5a97ff0061e0621e4a7f1967b617f
guidimarco <mark.88g@gmail.com>
2023-08-11 12:23:29 +0200

    Sidebar and Image Gallery Components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+import SideBar from '@/components/SideBar.vue'
+import ImageGallery from '@/components/ImageGallery.vue'
+
+const isWithColor = ref(true)
+
+const onSelectOption = (isColor) => {
+  isWithColor.value = isColor
+}
+</script>
+
 <template>
-  <div></div>
+  <v-app>
+    <side-bar @select-option="onSelectOption" />
+    <v-main>
+      <image-gallery :is-with-color="isWithColor" />
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-
-</script>
---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['isWithColor'])
+</script>
+
+<template>
+    <v-card class="mx-5 my-2 pa-3">
+        <v-row>
+            <v-col 
+                v-for="i in 200" cols="4" sm="3" md="2" lg="1">
+                <v-card>
+                    <v-img :src="`http://picsum.photos/500/300?image=${i * 5 + 10}${props.isWithColor ? '' : '&grayscale'}`"
+                        :lazy-src="`http://picsum.photos/10/6?image=${i * 5 + 10}`"
+                        aspect-ratio="1" cover>
+                        <template v-slot:placeholder>
+                            <v-row class="fill-height ma-0" align="center" justify="center">
+                                <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
+                            </v-row>
+                        </template>
+                    </v-img>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
